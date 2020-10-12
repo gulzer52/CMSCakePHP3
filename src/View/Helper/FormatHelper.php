db8f905966a78917cbf7d18331f21a72ced858c2
unknown <hgulzer@gmail.com>
2020-10-12 19:56:37 +0600

    my project

---
+++ src/View/Helper/FormatHelper.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\View\Helper;
+
+use Cake\View\Helper;
+use Cake\ORM\TableRegistry;
+
+class FormatHelper extends Helper{
+	public function getName($id, $table){
+		$record = TableRegistry::get($table);
+		$query = $record->find()->where(['id' => $id]);
+		return $query->first()->name;
+	}
+}
---
+++ tests/Fixture/AppointmentsFixture.php
@@ -0,0 +1,50 @@
+<?php
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * AppointmentsFixture
+ */
+class AppointmentsFixture extends TestFixture
+{
+    /**
+     * Fields
+     *
+     * @var array
+     */
+    // @codingStandardsIgnoreStart
+    public $fields = [
+        'id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'autoIncrement' => true, 'precision' => null],
+        'patient_id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'autoIncrement' => null],
+        'doctors_id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'autoIncrement' => null],
+        'appointment_date' => ['type' => 'date', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
+        'created' => ['type' => 'datetime', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
+        '_constraints' => [
+            'primary' => ['type' => 'primary', 'columns' => ['id'], 'length' => []],
+        ],
+        '_options' => [
+            'engine' => 'InnoDB',
+            'collation' => 'utf8mb4_general_ci'
+        ],
+    ];
+    // @codingStandardsIgnoreEnd
+    /**
+     * Init method
+     *
+     * @return void
+     */
+    public function init()
+    {
+        $this->records = [
+            [
+                'id' => 1,
+                'patient_id' => 1,
+                'doctors_id' => 1,
+                'appointment_date' => '2020-10-06',
+                'created' => '2020-10-06 16:05:53',
+            ],
+        ];
+        parent::init();
+    }
+}
